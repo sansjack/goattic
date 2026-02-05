dd147b24f19648a4005a56d6d5c50c7a5565229f
sansjack <contact@jacksansom.com>
2026-02-05 10:01:55 +0000

    feat: .env example + env construct updates

--- server/infra/src/vars.ts
+++ server/infra/src/vars.ts
@@ -4,6 +4,9 @@ const vars = z.object({
   ENV: z.enum(["dev", "prod"]).default("dev"),
   CDK_DEFAULT_ACCOUNT: z.string().optional(),
   CDK_DEFAULT_REGION: z.string().optional(),
+  DOMAIN_NAME: z.string(),
+  MEDIA_SUBDOMAIN: z.string(),
+  API_SUBDOMAIN: z.string(),
 });
 
 export const environmentVars = vars.parse(process.env);
